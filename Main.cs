01bdc6570e80b57d393ceb65c4d8489e7e642276
PunRathana <punrathana.pra@gmail.com>
2024-06-22 23:18:17 +0700

    add form login and main

---
+++ Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var formToOpen = new Login();
+            formToOpen.Show();
+        }
+        public class Login : Form2
+        {
+            
+        }
+    }
+}
--- loginForm.cs
+++ loginForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static Final.Main;
 
 namespace Final
 {
@@ -15,6 +16,44 @@ namespace Final
         public Form2()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            string phoneNumber = phone_number.Text;
+            string password = txtpassword.Text;
+
+            if (string.IsNullOrEmpty(phoneNumber) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please enter both phone number and password.", "Validation Error");
+                return;
+            }
+
+
+            this.Hide();
+
+            var mainToOpen = new main1();
+            mainToOpen.Show();
+
+        }
+        public class main1 : Main
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            phone_number.Text = string.Empty;
+            txtpassword.Text = string.Empty;
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
